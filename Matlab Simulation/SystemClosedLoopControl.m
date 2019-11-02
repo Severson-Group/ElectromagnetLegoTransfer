fc9e1c1f58eaad37318439160f4cc5913d376cd2
jtcomp <jcompas@wisc.edu>
2019-11-02 16:05:25 -0500

    add PID control simulation

--- Matlab Simulation/SystemClosedLoopControl.m
+++ Matlab Simulation/SystemClosedLoopControl.m
@@ -6,6 +6,7 @@ clear;
 Ks = 0.0028; % Sensor Gain
 dfdz = -0.1159; % Change in force per change in Z (cm)
 dfdi = 0.0422; % Change in force per amp
+dTdz = 0.0028; %Change in T (field intensity vs z (in cm)
 
 M = 0.0118; % Total Levitation mass (in Kg)
 
@@ -31,3 +32,42 @@ step(ClosedLoop_tf) ;
 title(['Z Step Responce with Kp = ',num2str(Kp)]);
 
 
+%% Unity feedback PID controller
+
+% Kd calculated off of Kp values
+Kp = 80;
+Kd = 20;
+Ki = 20;
+
+
+electromag_plant_tf = tf([dfdi/M] , [1 0 -dfdz/M]);
+s = tf('s');
+pid_controller_tf = Kp + Kd*s + Ki/s;
+
+G = electromag_plant_tf*pid_controller_tf;
+
+ClosedLoop_tf = G / (1 + G) ; 
+figure();
+[z,t] = step(ClosedLoop_tf) ; 
+plot(t,2.5*z);
+title(['Z Step Responce with Kp = ',num2str(Kp)]);
+
+%% Hall Effect Feedback
+
+% Kd calculated off of Kp values
+Kp = 80;
+Kd = 20;
+Ki = 20;
+
+
+electromag_plant_tf = tf([dfdi/M] , [1 0 -dfdz/M]);
+s = tf('s');
+pid_controller_tf = Kp + Kd*s + Ki/s;
+
+G = electromag_plant_tf*pid_controller_tf;
+
+ClosedLoop_tf = G / (1 + G) ; 
+figure();
+[z,t] = step(ClosedLoop_tf) ; 
+plot(t,2.5*z);
+title(['Z Step Responce with Kp = ',num2str(Kp)]);
